e6cbd8b422869d95dac1542d2c252cd506228c63
Matthew Sherman <msherman0801@gmail.com>
2019-01-26 01:40:29 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2019_01_25_190657) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
+    t.integer "release_date"
     t.string "director"
     t.string "lead"
     t.boolean "in_theaters"
